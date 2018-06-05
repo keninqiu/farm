e226ce3afc772878b8ad156dc4b72931cb3c6aea
Ken Qiu <kqiu@drmg.ca>
2018-06-05 18:41:32 -0400

    fix

--- dist/start.js
+++ dist/start.js
@@ -541,7 +541,12 @@ var GameMain = /** @class */ (function () {
         Laya.stage.alignH = Laya.Stage.ALIGN_CENTER;
         Laya.stage.alignV = Laya.Stage.ALIGN_MIDDLE;
         Laya.stage.scaleMode = "showall";
-        Laya.stage.screenMode = Laya.Stage.SCREEN_HORIZONTAL;
+        if (Laya.Browser.clientWidth < Laya.Browser.clientHeight) {
+            Laya.stage.screenMode = Laya.Stage.SCREEN_VERTICAL;
+        }
+        else {
+            Laya.stage.screenMode = Laya.Stage.SCREEN_HORIZONTAL;
+        }
         Laya.stage.fullScreenEnabled = true;
     }
     GameMain.prototype.initSprites = function () {
--- src/start.ts
+++ src/start.ts
@@ -127,7 +127,13 @@ class GameMain{
         Laya.stage.alignV = Laya.Stage.ALIGN_MIDDLE;
 
         Laya.stage.scaleMode = "showall";
-        Laya.stage.screenMode = Laya.Stage.SCREEN_HORIZONTAL;
+        
+        if(Laya.Browser.clientWidth < Laya.Browser.clientHeight) {
+            Laya.stage.screenMode = Laya.Stage.SCREEN_VERTICAL;
+        }
+        else {
+            Laya.stage.screenMode = Laya.Stage.SCREEN_HORIZONTAL;
+        }
         Laya.stage.fullScreenEnabled = true;
 
     }
