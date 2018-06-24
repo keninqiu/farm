ad76e42898e315f1c32ea9082a1ea3275a107004
Ken Qiu <kqiu@drmg.ca>
2018-06-24 07:31:05 -0400

    fix

--- res/js/main.js
+++ res/js/main.js
@@ -2,3 +2,15 @@ function closeMovie() {
   player.dispose(); //销毁
   $('#J_prismPlayer').empty();//id为html里指定的播放器的容器id
 }
+
+var app = new Vue({
+        el: '#app',
+        data: {
+            visible: false
+        },
+        methods: {
+            show: function () {
+                this.visible = true;
+            }
+        }
+    })
